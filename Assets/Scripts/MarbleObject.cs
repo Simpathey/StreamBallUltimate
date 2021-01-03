c6ca4fb1df91908210a22f9d89d59bba403c6eaa
Simpathey <62914757+Simpathey@users.noreply.github.com>
2021-01-03 11:27:26 -0800

    100 possible
    oops

--- Assets/Scripts/MarbleObject.cs
+++ Assets/Scripts/MarbleObject.cs
@@ -58,7 +58,7 @@ public class MarbleObject : MonoBehaviour
     public void LongJump()
     {
         //How far player jumps
-        int percentage = UnityEngine.Random.Range(0, 10000);
+        int percentage = UnityEngine.Random.Range(0, 10001);
         if (percentage == 10000)
         {
             commands = FindObjectOfType<Commands>();
