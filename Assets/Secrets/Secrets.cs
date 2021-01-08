2ded457f660b30a46367df1875af827c6fa18ab7
HelpfulStranger999 <helpfulstranger999@gmail.com>
2021-01-07 23:45:29 -0600

    Refactors the names and removes the need for a method to be called

--- Assets/Secrets/Secrets.cs
+++ Assets/Secrets/Secrets.cs
@@ -5,36 +5,9 @@ using UnityEngine;
 
 public static class Secrets
 {
-    public static string client_id;
-    public static string client_secret; 
-    public static string bot_access_token; 
-    public static string bot_refresh_token; //hai
-    public static void EnvironmentVariables()
-    {
-        client_id = Environment.GetEnvironmentVariable("client_id");
-        client_secret = Environment.GetEnvironmentVariable("client_secret");
-        bot_access_token = Environment.GetEnvironmentVariable("bot_access_token");
-        bot_refresh_token = Environment.GetEnvironmentVariable("bot_refresh_token");
-        /*
-        string value;
-        bool toDelete = false;
+    public static string ClientID { get; } = Environment.GetEnvironmentVariable("client_id");
+    public static string ClientSecret { get; } = Environment.GetEnvironmentVariable("client_secret");
+    public static string BotAccessToken { get; } = Environment.GetEnvironmentVariable("bot_access_token");
+    public static string BotRefreshToken { get; } = Environment.GetEnvironmentVariable("bot_refresh_token"); //hai
 
-        // Check whether the environment variable exists.
-        value = Environment.GetEnvironmentVariable("client_id");
-        // If necessary, create it.
-        if (value == null)
-        {
-            Environment.SetEnvironmentVariable("client_id", "");
-            Environment.SetEnvironmentVariable("client_secret", "");
-            Environment.SetEnvironmentVariable("bot_access_token", "");
-            Environment.SetEnvironmentVariable("bot_refresh_token", "");
-            toDelete = true;
-
-            // Now retrieve it.
-            client_id = Environment.GetEnvironmentVariable("client_id");
-            client_secret = Environment.GetEnvironmentVariable("client_secret");
-            bot_access_token = Environment.GetEnvironmentVariable("bot_access_token");
-            bot_refresh_token = Environment.GetEnvironmentVariable("bot_refresh_token");
-   */
-    }
 }
